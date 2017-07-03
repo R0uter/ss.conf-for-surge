e8b8bcfb6fee720c4d7d1672cff25f0094e6682f
R0uter <lomyr.jo@gmail.com>
2017-07-03 14:29:57 +0800

    new adlist url from lhie1/Surge

--- ssconf.py
+++ ssconf.py
@@ -104,7 +104,7 @@ def getAdList():
     # the url of  https://github.com/lhie1/Surge
     outfile = './list/adlist.txt'
     tmpfile = './list/adtmp'
-    baseurl = 'https://github.com/lhie1/Surge/raw/master/Surge.conf'
+    baseurl = 'https://github.com/lhie1/Surge/raw/master/Rule_REJECT'
 
     comment_pattern = '^\!|\[|^@@|\/|http|\#|\*|\?|\_|^\.|^\d+\.\d+\.\d+\.\d+'
     domain_pattern = '(\#?[\w\-\_]+\,[\/\w\.\-\_]+\,REJECT)[\/\*]*'
