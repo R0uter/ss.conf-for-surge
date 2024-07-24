caf4c93bf6f41d9252de109e287d9ca58d068f8c
R0uter <lomyr.jo@gmail.com>
2024-07-24 11:33:55 +1000

    fix the pipeline

--- ssconf.py
+++ ssconf.py
@@ -34,15 +34,15 @@ def white_list_check():
     except:
         print('Get list update failed,use cache to update instead.')
 
-    whitelist = codecs.open('./list/whitelist','r','utf-8')
-    whitelistTxt = codecs.open('./list/whitelist.txt','w','utf-8')
+    whitelist = codecs.open('./list/whitelist', 'r', 'utf-8')
+    whitelistTxt = codecs.open('./list/whitelist.txt', 'w', 'utf-8')
     whitelistTxt.write('// updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S" + '\n'))
     # Write list
     for line in whitelist.readlines():
-        
+
         domain = re.findall(r'(?<=")[a-z0-9|\-]+\.\w+', line)
         if len(domain) > 0:
-            whitelistTxt.write('DOMAIN-SUFFIX,%s,DIRECT\n'%(domain[0]))
+            whitelistTxt.write('DOMAIN-SUFFIX,%s,DIRECT\n' % (domain[0]))
 
     whitelist.close()
     whitelistTxt.close()
@@ -52,8 +52,8 @@ def get_gfw_list():
     # the url of gfwlist
     base_url = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
 
-    comment_pattern = '^\!|\[|^@@|^\d+\.\d+\.\d+\.\d+'
-    domain_pattern = '([\w\-\_]+\.[\w\.\-\_]+)[\/\*]*'
+    comment_pattern = r'^\!|\[|^@@|^\d+\.\d+\.\d+\.\d+'
+    domain_pattern = r'([\w\-\_]+\.[\w\.\-\_]+)[\/\*]*'
 
     tmp_file = './list/tmp'
     gfwListTxt = codecs.open('./list/gfwlist.txt', 'w+', 'utf-8')
@@ -80,35 +80,35 @@ def get_gfw_list():
     # Write list
     for line in tfs.readlines():
 
-     if re.findall(comment_pattern, line):
-         continue
-     else:
-         domain = re.findall(domain_pattern, line)
-         if domain:
-             try:
-                 found = domainList.index(domain[0])
-             except ValueError:
-                 domainList.append(domain[0])
-                 gfwListTxt.write('DOMAIN-SUFFIX,%s,Proxy,force-remote-dns\n' % (domain[0]))
-         else:
-             continue
+        if re.findall(comment_pattern, line):
+            continue
+        else:
+            domain = re.findall(domain_pattern, line)
+            if domain:
+                try:
+                    found = domainList.index(domain[0])
+                except ValueError:
+                    domainList.append(domain[0])
+                    gfwListTxt.write('DOMAIN-SUFFIX,%s,Proxy,force-remote-dns\n' % (domain[0]))
+            else:
+                continue
 
     tfs.close()
     gfwListTxt.close()
 
 
 def get_ad_list():
-    # get list to block most of ads .
-    # the url of  https://github.com/lhie1/Surge
+    # get list to block most of the ads.
+    # the url of  https://github.com/zhouzhouprogram/Shadowrocket-ADBlock-Rules
     outfile = './list/adlist.txt'
     tmpfile = './list/adtmp'
-    baseurl = 'https://github.com/lhie1/Rules/raw/master/Quantumult/Quantumult.conf'
+    baseurl = 'https://raw.githubusercontent.com/zhouzhouprogram/Shadowrocket-ADBlock-Rules/master/sr_proxy_banad.conf'
 
-    comment_pattern = '^\!|\[|^@@|\/|http|\#|\*|\?|\_|^\.|^\d+\.\d+\.\d+\.\d+'
-    domain_pattern = '(\#?[\w\-\_]+\,[\/\w\.\-\_]+\,REJECT)[\/\*]*'
+    comment_pattern = r'^\!|\[|^@@|\/|http|\#|\*|\?|\_|^\.|^\d+\.\d+\.\d+\.\d+'
+    domain_pattern = r'.+,.+,(REJECT|Reject)'
 
     fs = codecs.open(outfile, 'w', 'utf-8')
-    fs.write('// thx  https://github.com/lhie1/Surge \n')
+    fs.write('// thx  https://github.com/zhouzhouprogram/Shadowrocket-ADBlock-Rules \n')
     fs.write('// updated on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '\n')
     fs.write('\n')
     try:
@@ -120,7 +120,7 @@ def get_ad_list():
         tfs.write(content)
         tfs.close()
         print('adlist fetched, writing...')
-        
+
     except:
         print('adlist fetch failed, use tmpfile instead...')
     # Store all domains, deduplicate records
@@ -129,28 +129,28 @@ def get_ad_list():
     # Write list
     tfs = codecs.open(tmpfile, 'r', 'utf-8')
     for line in tfs.readlines():
-
+        line = line.strip()
         if re.findall(comment_pattern, line):
             continue
+
+        if re.findall(domain_pattern, line):
+            try:
+                found = domainlist.index(line)
+            except ValueError:
+                line = line.replace('Reject', 'REJECT')
+                domainlist.append(line)
+                fs.write(line+'\n')
         else:
-            domain = re.findall(domain_pattern, line)
-            if domain:
-                try:
-                    found = domainlist.index(domain[0])
-                except ValueError:
-                    domainlist.append(domain[0])
-                    fs.write(domain[0] + '\n')
-            else:
-                continue
+            continue
 
     tfs.close()
     fs.close()
 
 
 def gen_gfw_conf():
-    f = codecs.open('template/ss_gfwlist_conf', 'r','utf-8')
-    gfw_list = codecs.open('list/gfwlist.txt', 'r','utf-8')
-    ad_list = codecs.open('list/adlist.txt', 'r','utf-8')
+    f = codecs.open('template/ss_gfwlist_conf', 'r', 'utf-8')
+    gfw_list = codecs.open('list/gfwlist.txt', 'r', 'utf-8')
+    ad_list = codecs.open('list/adlist.txt', 'r', 'utf-8')
     proxy = codecs.open('ServerConfig.txt', 'r', 'utf-8-sig')
     file_content = f.read()
     ad_list_buffer = ad_list.read()
@@ -188,7 +188,7 @@ def gen_white_conf():
     file_content = file_content.replace('__GFWWHITELIST__', gfw_list_buffer)
     file_content = file_content.replace('__Proxy__', proxy_buffer)
 
-    confs = codecs.open('configFileHere/whitelist.conf', 'w','utf-8')
+    confs = codecs.open('configFileHere/whitelist.conf', 'w', 'utf-8')
     confs.write(file_content)
     confs.close()
 
@@ -249,7 +249,7 @@ def main():
     print('Generate back to china config file: cn.conf')
     gen_cn_conf()
     print('All done!')
-    print('Now you need edit config file to add your server infomation.')
+    print('Now you need edit config file to add your server information.')
 
 
 if __name__ == '__main__':
--- surge_rule_set.py
+++ surge_rule_set.py
@@ -2,18 +2,19 @@ import codecs
 from shutil import copyfile
 import os, sys
 
+
 def txt2list(src, dst, removed_str):
-  print('Generate surge rule-set ' + dst)
-  fp = open(dst,'w') 
-  lines = open(src).readlines()  #打开文件，读入每一行
-  for s in lines:
-    fp.write( s.replace(removed_str,''))
-  fp.close()
-  print(dst + ' ' + 'done')
+    print('Generate surge rule-set ' + dst)
+    fp = open(dst, 'w')
+    lines = open(src).readlines()  #打开文件，读入每一行
+    for s in lines:
+        fp.write(s.replace(removed_str, ''))
+    fp.close()
+    print(dst + ' ' + 'done')
+
 
 txt2list('list/adlist.txt', 'list/adlist.list', ',REJECT')
 txt2list('list/whitelist.txt', 'list/whitelist.list', ',DIRECT')
 txt2list('list/gfwlist.txt', 'list/gfwlist.list', ',Proxy,force-remote-dns')
 
 print('Generate surge rule-set complele')
-
